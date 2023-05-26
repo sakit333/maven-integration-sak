2df6705aada1681dcd246f6a8994128320523657
ajit40 <ajitseenu409@gmail.com>
2023-05-26 19:42:27 +0530

    extra method

--- src/main/java/jenkins/Sample.java
+++ src/main/java/jenkins/Sample.java
@@ -2,5 +2,10 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("new file");
 		System.out,println("happy moments");
+		add();
+	}
+	public static void add()
+	{
+		System.out.println("sumne printed");
 	}
 }
