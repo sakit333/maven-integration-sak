782e05082d830cf2b2daad84a0d57016ac66edff
ajit40 <ajitseenu409@gmail.com>
2023-05-26 19:44:29 +0530

    msg for mistakes is done

--- src/main/java/jenkins/Sample.java
+++ src/main/java/jenkins/Sample.java
@@ -1,7 +1,7 @@
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("new file");
-		System.out,println("happy moments");
+		System.out.println("happy moments");
 		add();
 	}
 	public static void add()
